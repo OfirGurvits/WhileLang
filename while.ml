13881b2a441f9af70d2e2970703d4ccbc455d80b
OfirGurvits <ofirgurvits@gmail.com>
2024-01-14 15:41:41 +0200

    added dict for vars

--- while.ml
+++ while.ml
@@ -1,26 +1,25 @@
 type num = Int
-
 type var = string
+let variables = Hashtbl.create 20;;
 
 type a_expr =
-  | Plus of a_expr * a_expr        
-  | Minus of a_expr * a_expr
-  | Times of a_expr * a_expr
-  | Var of var 
-  | Num of num
+| Plus of a_expr * a_expr        
+| Minus of a_expr * a_expr
+| Times of a_expr * a_expr
+| Var of var 
+| Num of num
 
 type b_exp =
-  | true
-  | false
-  | Not of b_exp
-  | And of b_exp * b_exp
-  | Equal of a_expr * a_expr
-  | GreaterThan of a_expr * a_expr
+| true
+| false
+| Not of b_exp
+| And of b_exp * b_exp
+| Equal of a_expr * a_expr
+| GreaterThan of a_expr * a_expr
 
 type stm = 
-  | Assign of var * a_expr
-  | Skip
-  | EndLine of stm * stm
-  | If of b_exp * stm * stm
-  | While of b_exp * stm
-  
+| Assign of var * a_expr
+| Skip
+| EndLine of stm * stm
+| If of b_exp * stm * stm
+| While of b_exp * stm
